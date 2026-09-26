1a174f11ca32afa563a94b12c468ebebf7257acf
sudairkhan3455-cmyk <sudairkhan3455@gmail.com>
2026-09-26 09:38:17 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>My App</h1>
-      <p>App is working!</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
